50760845050dae8dc672c1ce3a1c13117301551d
aecollier <aecollier@email.arizona.edu>
2021-06-09 21:57:11 -0700

    Update files

--- trending.py
+++ trending.py
@@ -7,11 +7,12 @@ results = 10
 
 # Since TikTok changed their API you need to use the custom_verifyFp option. 
 # In your web browser you will need to go to TikTok, Log in and get the s_v_web_id value.
-trending = api.trending(count=results, custom_verifyFp='verify_kp1smpu7_udw29DoR_RhXe_4p8C_B8Q9_RukzGDZGlesz')
+trending = api.trending(count=results, custom_verifyFp='verify_kpqf4sp4_SoSmCG3K_JAJv_4zah_8n9j_xl4cG9vZdGDc')
 # From first meeting with Morgan: verify_kon3g2ik_kTgoMHHt_7KGC_4kss_BQrY_bLRTl6saCDDH
 
 for tiktok in trending:
     # Prints the id of the tiktok
-    authors = print(tiktok['author']['uniqueId'])
+    print(tiktok['music']['title'],':',tiktok['music']['id'])
+
 
 #print(len(trending))
