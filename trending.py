b046bd1256f48b379e170ae9e3f09b6357719a67
aecollier <aecollier@email.arizona.edu>
2021-06-24 09:34:02 -0700

    Added naming convention

--- trending.py
+++ trending.py
@@ -1,18 +1,33 @@
 from TikTokApi import TikTokApi
-api = TikTokApi.get_instance()
+import json
+from datetime import datetime
+
+v = 'verify_kq8ugckw_iIUbvyId_SYn0_45ie_BlZf_haGHX8GrPhpd'
+api = TikTokApi.get_instance(custom_verifyFp=v, use_test_endpoints = True)
 # If playwright doesn't work for you try to use selenium
 # api = TikTokApi.get_instance(use_selenium=True)
 
-results = 10
+results = 1999
+time = datetime.now()
+current = time.strftime("%d%m%Y_%H%M%S")
+
 
 # Since TikTok changed their API you need to use the custom_verifyFp option. 
 # In your web browser you will need to go to TikTok, Log in and get the s_v_web_id value.
-trending = api.trending(count=results, custom_verifyFp='verify_kpqf4sp4_SoSmCG3K_JAJv_4zah_8n9j_xl4cG9vZdGDc')
-# From first meeting with Morgan: verify_kon3g2ik_kTgoMHHt_7KGC_4kss_BQrY_bLRTl6saCDDH
+trending = api.trending(count=results)
+# verify_de3299f562a9f9ed9b52564fc0522962
+# verify_de3299f562a9f9ed9b52564fc0522962
 
+# From first meeting with Morgan: verify_kon3g2ik_kTgoMHHt_7KGC_4kss_BQrY_bLRTl6saCDDH
+file_name = 'trending_'+current
+with open(file_name,'w') as outfile:
+    json.dump(trending, outfile)
+
+users = []
 for tiktok in trending:
     # Prints the id of the tiktok
-    print(tiktok['music']['title'],':',tiktok['music']['id'])
+    users.append(tiktok['author']['id'])
+print(users)
 
 
 #print(len(trending))
