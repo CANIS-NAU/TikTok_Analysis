4d56e409a6d11bf0a575995dc0d68a0b18db3bc5
aecollier <aecollier@email.arizona.edu>
2021-06-24 09:34:15 -0700

    Trying to get around captcha

---
+++ user_test.py
@@ -0,0 +1,9 @@
+from TikTokApi import TikTokApi as t
+v = 'verify_kqakq8wj_IWhiFiNW_XCHW_46H2_BcWG_UWLNujuBLkJI'
+api = t.get_instance(custom_verifyFp=v)
+k = api.by_username("tiamiscihk")
+print(k)
+
+'verify_khr3jabg_V7ucdslq_Vrw9_4KPb_AJ1b_Ks706M8zIJTq'
+
+'verify_kqakq8wj_IWhiFiNW_XCHW_46H2_BcWG_UWLNujuBLkJI'
